37693bf597ad31710d85d7075cd92bc90c2eb86b
Thang <thangtedy@gmail.com>
2025-12-29 22:55:51 +0700

    update setting email

--- src/main/java/com/petlytic/configs/EmailConfiguration.java
+++ src/main/java/com/petlytic/configs/EmailConfiguration.java
@@ -10,16 +10,23 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfiguration {
+    @Value("${spring.mail.host}")
+    private String emailHost;
+
+    @Value("${spring.mail.port}")
+    private int emailPort;
+
     @Value("${spring.mail.username}")
     private String emailUsername;
+
     @Value("${spring.mail.password}")
     private String emailPassword;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost(emailHost);
+        mailSender.setPort(emailPort);
         mailSender.setUsername(emailUsername);
         mailSender.setPassword(emailPassword);
 
